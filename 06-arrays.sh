c59a8932efab12462f8d4bbb9e2cf617e4d3901b
GudipatiVenkataPrasad <surya774444@gmail.com>
2025-01-27 06:08:41 -0800

    time adding script

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -2,6 +2,6 @@
 
 FRUITS=("Aplle" "Grapes" "Banana")
 
-echo "First value : $FRUITS[0]"
-echo "First value : $FRUITS[1]"
-echo "First value : $FRUITS[2]"
+echo "First value : ${FRUITS[0]}"
+echo "Second value : ${FRUITS[1]}"
+echo "Third value : ${FRUITS[2]}"

b15acbe5b42a4f5d7809cbd410c8c05faec9995d
GudipatiVenkataPrasad <surya774444@gmail.com>
2025-01-27 04:41:45 -0800

    3nd adding script

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ram
+PERSON2=venky
+
+echo "$PERSON1 : Hello $PERSON2,good morning"
+echo "$PERSON2 : Hi $PERSON1 ,very good morning"
+echo "$PERSON1 : How are you $PERSON2"
+echo "$PERSON2 : I am fine ,what about you"

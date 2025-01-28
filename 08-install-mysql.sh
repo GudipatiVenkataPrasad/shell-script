bec63dcc344115555d3ef9a0dcaa070508d56e74
GudipatiVenkataPrasad <surya774444@gmail.com>
2025-01-28 09:34:34 -0800

    adding condition statement

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -24,3 +24,16 @@ else
     echo "Installing mysql is scuess"s
 
 fi
+
+yum install git -y
+
+
+
+if [ $? -ne 0 ]
+then 
+    echo "ERROR :: Installing GIT is failed"
+    exit 1
+else 
+    echo "Installing GIT is scuess"s
+
+fi

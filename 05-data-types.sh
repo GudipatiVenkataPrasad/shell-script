c2175006bbfb9dfde37c180ec2b56eb2ac2f230b
GudipatiVenkataPrasad <surya774444@gmail.com>
2025-01-28 10:09:56 -0800

    adding condition statement

--- 05-data-types.sh
+++ 05-data-types.sh
@@ -8,5 +8,5 @@ SUM=$((NUMBER1+NUMBER2))
 echo "Total :: $SUM"
 
 echo "How many arugs are passed:: $@"
-echo "All args are passed :: $#'
-echo "Script name :: $0"
+echo "All args are passed :: $#"
+echo  "Script name :: $0"

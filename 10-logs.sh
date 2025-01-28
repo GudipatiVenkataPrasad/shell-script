1f9cf8e473a7229c0ec45a6b4ab0cd2e63bb77ab
GudipatiVenkataPrasad <surya774444@gmail.com>
2025-01-28 10:04:44 -0800

    adding condition statement

---
+++ 10-logs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo "script name :: $0"
+
+VALIDATE()
+{
+
+    if [ $? -ne 0 ]
+    then 
+        echo "ERROR :: $2.. failed"
+        exit 1
+    else 
+        echo "$2... scuess"s
+
+    fi
+}
+
+
+if [ $? -ne 0 ]
+then 
+    echo "ERROR :: Installing Mysql is failed"
+    exit 1
+else 
+    echo "Installing mysql is scuess"s
+
+fi
+yum install mysql -y
+VALIDATE $? "Installing Mysql"
+
+yum install git -y
+VALIDATE $? "Installing git"
